76e39d988af3c351ad1d0f7b38b2c10cef49fd88
Clashnewbme <amoryjamesmonson@icloud.com>
2026-03-04 17:57:35 +0800

    Update memory.h

--- FreezeProject/memory.h
+++ FreezeProject/memory.h
@@ -2,10 +2,19 @@
 #define MEMORY_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 extern uint32_t total_memory;
 extern uint32_t used_memory;
+extern uint32_t free_memory;
 
-void* kmalloc(uint32_t size);
+void memory_init(uint32_t mem_size);
+
+void* kmalloc(size_t size);
+void* kmalloc_aligned(size_t size, uint32_t alignment);
+void  kfree(void* ptr);
+
+uint32_t memory_get_used(void);
+uint32_t memory_get_free(void);
 
 #endif
